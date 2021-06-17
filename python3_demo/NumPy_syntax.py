d93e959a3c6d87a748444184a7a2d0fed03df24b
shenxw <>
2021-06-17 21:46:55 +0800

    merge

--- python3_demo/NumPy_syntax.py
+++ python3_demo/NumPy_syntax.py
@@ -272,16 +272,36 @@ d[0,0] = 9999
 #
 # 应用广播法则之后，所有数组的大小必须匹配。
 
-a = arange(10)
-max = -float("inf")
-count = 0
-for i in range(len(a)):
-    if max<a[i]:
-        max= a[i]
-        count+=1
-        print(count)
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+trainset = torchvision.datasets.CIFAR10(root='./data',train=True,download=True,transform=transform)
+trainload = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data',train=False,download=True,transform=transform)
+testload = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)
+
+classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
 
+
+if __name__ == '__main__':
+    dataiter = iter(trainload)
+    images, labels = dataiter.next()
+    imshow(torchvision.utils.make_grid(images))
+    print('  '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
 
